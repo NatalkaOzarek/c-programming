701eaef1a36750c4723a6b17ab8f3d8b1ad4d8d1
Natalia <63668961+NatalkaOzarek@users.noreply.github.com>
2021-01-10 23:08:26 +0100

    Add files via upload

---
+++ zajecia_3/obwody.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    char f;
+    scanf("%c", &f);
+    if(f= 'p');{
+        scanf("%i %i", &a, &b);
+        printf("%i \n",  a*b);
+        return 0;
+    }
+    if(f= 'k');{
+        scanf("%i",&a);
+        printf("%i \n",  a*a);
+        return 0;
+    }
+     if(f= 't');{
+        scanf("%i %i ",&a,&b);
+        printf("%i \n" , a*b/2);
+        return 0;
+    }
+       
+
+}
